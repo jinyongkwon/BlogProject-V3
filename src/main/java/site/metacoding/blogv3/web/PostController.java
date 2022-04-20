bd2c538bccae483857c0a321a8d4c2e9a7a4a32d
jinyongkwon <wlsdyd1178@naver.com>
2022-04-20 16:40:36 +0900

    카테고리 목록보기 완료

--- src/main/java/site/metacoding/blogv3/web/PostController.java
+++ src/main/java/site/metacoding/blogv3/web/PostController.java
@@ -1,14 +1,26 @@
 package site.metacoding.blogv3.web;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import lombok.RequiredArgsConstructor;
+import site.metacoding.blogv3.config.auth.LoginUser;
+import site.metacoding.blogv3.service.PostService;
+import site.metacoding.blogv3.web.dto.post.PostRespDto;
+
+@RequiredArgsConstructor
 @Controller
 public class PostController {
 
+    private final PostService postService;
+
     @GetMapping("/user/{id}/post")
-    public String postList(@PathVariable Integer id) {
+    public String postList(@PathVariable Integer id, @AuthenticationPrincipal LoginUser loginUser, Model model) {
+        PostRespDto postRespDto = postService.게시글목록보기(id);
+        model.addAttribute("postRespDto", postRespDto);
         return "/post/list";
     }
 
